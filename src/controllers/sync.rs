43394add5809ad36cac86134b3a28809fa99f596
poulp <amathieu@protonmail.com>
2017-07-17 14:55:20 +0200

    Add atom support

--- src/controllers/sync.rs
+++ src/controllers/sync.rs
@@ -1,10 +1,16 @@
-extern crate rusqlite;
+extern crate reqwest;
 extern crate rss;
+extern crate rusqlite;
+extern crate atom_syndication;
 
 use std::thread;
+use std::io::Read;
+use std::io::Error;
+use std::str::FromStr;
 
 use self::rusqlite::Connection;
 use self::rss::Channel;
+use self::atom_syndication::Feed;
 
 use controllers::component::Component;
 use app::Cache;
@@ -13,6 +19,37 @@ use database::{
     get_channels,
     create_item
 };
+
+fn content_from_url(link: &str) -> Result<String, FetchError> {
+    /// Download content
+    let mut content = String::new();
+    reqwest::get(link)?.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn update_channel(db_connection: &Connection, title: &str, description: &str, channel_id: i32) {
+    db_connection.execute(
+        "UPDATE channel SET title = ?, description = ? WHERE channel_id = ?",
+        &[&title, &description, &channel_id]
+    ).unwrap();
+}
+
+pub enum FetchError {
+    UrlRequest(reqwest::Error),
+    Io(Error)
+}
+
+impl From<reqwest::Error> for FetchError {
+    fn from(err: reqwest::Error) -> FetchError {
+        FetchError::UrlRequest(err)
+    }
+}
+
+impl From<Error> for FetchError {
+    fn from(err: Error) -> FetchError {
+        FetchError::Io(err)
+    }
+}
 
 pub struct ControllerSync {
 }
@@ -37,6 +74,7 @@ impl Component for ControllerSync {
 
     fn on_synchronize_start(&mut self, cache: &mut Cache) {
         /// Fetch channels and save items
+        /// RSS and ATOM support
         let tx = cache.tx.clone();
         // TODO one thread per channel
         thread::spawn(move || {
@@ -47,29 +85,70 @@ impl Component for ControllerSync {
                 tx.send(
                     format!("{}", channel.title())
                 ).unwrap();
-                // Download feeds
-                let channel_opt = Channel::from_url(channel.link.as_ref());
-                match channel_opt {
-                    Ok(channel_fetched) => {
-                        db_conn.execute(
-                            "UPDATE channel SET title = ?, description = ? WHERE channel_id = ?",
-                            &[&channel_fetched.title(), &channel_fetched.description(), &channel.id]
-                        ).unwrap();
+                // Download items
+                let channel_content = content_from_url(channel.link.as_ref());
 
-                        /* Fetch feeds */
-                        for item in channel_fetched.items() {
-                            /* Save feed in db */
-                            create_item(
-                                &db_conn,
-                                item.link().unwrap(),
-                                item.title().unwrap(),
-                                item.description().unwrap(),
-                                channel.id);
+                match channel_content {
+                    Ok(content) => {
+                        // Try RSS
+                        let channel_rss_result = Channel::from_str(content.as_ref());
+                        match channel_rss_result {
+                            Ok(channel_rss) => {
+                                update_channel(
+                                    &db_conn,
+                                    &channel_rss.title(),
+                                    &channel_rss.description(),
+                                    channel.id
+                                );
+                                /* Fetch feeds */
+                                for item in channel_rss.items() {
+                                    /* Save feed in db */
+                                    if let (Some(link), Some(title), Some(description)) =
+                                           (item.link(), item.title(), item.description()) {
+                                        create_item(
+                                            &db_conn,
+                                            link,
+                                            title,
+                                            description,
+                                            channel.id);
+                                    }
+                                }
+                            },
+                            Err(_) => {
+                                // Try Atom
+                                let channel_atom_result = Feed::from_str(content.as_ref());
+                                match channel_atom_result {
+                                    Ok(channel_atom) => {
+                                        update_channel(
+                                            &db_conn,
+                                            &channel_atom.title(),
+                                            &channel_atom.subtitle().unwrap_or(""),
+                                            channel.id
+                                        );
+                                        /* Fetch entries */
+                                        for entry in channel_atom.entries() {
+                                            if let Some(content) = entry.content() {
+                                                let link_opt = entry.links().into_iter().find(|&x| x.rel() == "self");
+                                                if let (Some(link), title, Some(value)) =
+                                                       (link_opt, entry.title(), content.value()) {
+                                                    create_item(
+                                                        &db_conn,
+                                                        link.href(),
+                                                        title,
+                                                        value,
+                                                        channel.id);
+                                                }
+                                            }
+                                        }
+                                    },
+                                    Err(_) => {}
+                                }
+                            }
                         }
-
                     },
-                    Err(_error) => {}
-                };
+                    Err(_) => {}
+                }
+
                 tx.send(
                     format!("cdone")
                 ).unwrap();
